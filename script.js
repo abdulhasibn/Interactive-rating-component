655bccdf0e750c29253b2fc32cd24ac0ace42ad8
Abdul Hasib <abdulhasibn@gmail.com>
2022-09-22 12:10:55 +0530

    Adds rating to the thans state dynamically

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const ratingButtons = document.querySelectorAll(".normal");
-const ratingButtonsArr = Array.from(ratingButtons);
-console.log(ratingButtonsArr);
-ratingButtonsArr.forEach((button) => {
-  button.addEventListener("click", () => {
-    console.log(button);
-    button.classList.toggle("clicked_state");
-  });
-});
-
 const submitButton = document.querySelector(".submit_btn");
 submitButton.addEventListener("click", () => {
   const thankYouState = document.querySelector(".thank_you_state");
   thankYouState.classList.toggle("visible_thanks");
   const ratingState = document.querySelector(".rating_state");
   ratingState.classList.add("invisible_rating");
+});
+const ratingResult = document.querySelector(".rating_result");
+const ratingButtons = document.querySelectorAll(".normal");
+const ratingButtonsArr = Array.from(ratingButtons);
+let ratingValue;
+ratingButtonsArr.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button);
+    ratingValue = button.value;
+    ratingResult.innerText = `You selected ${ratingValue} out of 5`;
+  });
 });

b9399249a2f733eae512070eb1d2014c835bb208
Abdul Hasib <abdulhasibn@gmail.com>
2022-09-22 07:23:32 +0530

    Add thank You state

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const ratingButtons = document.querySelectorAll(".clicked_state");
+const ratingButtons = document.querySelectorAll(".normal");
 const ratingButtonsArr = Array.from(ratingButtons);
 console.log(ratingButtonsArr);
 ratingButtonsArr.forEach((button) => {
   button.addEventListener("click", () => {
     console.log(button);
-    button.classList.toggle("normal");
+    button.classList.toggle("clicked_state");
   });
+});
+
+const submitButton = document.querySelector(".submit_btn");
+submitButton.addEventListener("click", () => {
+  const thankYouState = document.querySelector(".thank_you_state");
+  thankYouState.classList.toggle("visible_thanks");
+  const ratingState = document.querySelector(".rating_state");
+  ratingState.classList.add("invisible_rating");
 });
